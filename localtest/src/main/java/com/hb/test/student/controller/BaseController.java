8dd672bc2a635b28aea32654ea6fe3f00150227f
tuboshu1986 <zzhbce321@163.com>
2019-07-25 21:33:22 +0800

    位置 查看页面

--- localtest/src/main/java/com/hb/test/student/controller/BaseController.java
+++ localtest/src/main/java/com/hb/test/student/controller/BaseController.java
@@ -2,44 +2,41 @@ package com.hb.test.student.controller;
 
 import com.hb.test.student.model.BaseModel;
 import com.hb.test.student.service.BaseService;
-import org.hibernate.sql.Update;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
 
 public abstract class BaseController<T extends BaseModel, K extends Serializable> {
     @GetMapping("/{key}")
-    public String get(@PathVariable("key") K key, Model model){
+    public String get(@PathVariable("key") K key, ModelMap model){
         model.addAttribute("beandData", getService().get(key));
         model.addAttribute("beandOptType", OptType.VIEW);
-        return pre() + "/model.html";
+        return pre() + "/model";
     }
 
     @PostMapping
-    public String post(T param, Model model, OptType optType){
+    public String post(T param, ModelMap model, OptType optType){
         T rstBean = getService().update(param);
         model.addAttribute("beandData", rstBean);
         model.addAttribute("beandOptType", OptType.UPDATE);
-        return pre() + "/model.html";
+        return pre() + "/model";
     }
 
     @PutMapping
-    public String put(T param, Model model){
+    public String put(T param, ModelMap model){
         T rstBean = getService().add(param);
         model.addAttribute("beandData", rstBean);
         model.addAttribute("beandOptType", OptType.ADD);
-        return pre() + "/model.html";
+        return pre() + "/model";
     }
 
     @DeleteMapping("/{key}")
-    public String delete(@PathVariable("key") K key, Model model){
+    public String delete(@PathVariable("key") K key, ModelMap model){
         T rstBean = getService().delete(key);
         model.addAttribute("beandData", rstBean);
         model.addAttribute("beandOptType", OptType.DELETE);
-        return pre() + "/list.html";
+        return pre() + "/list";
     }
 
     @RequestMapping(value = "/page", method = RequestMethod.POST)
--- localtest/src/main/java/com/hb/test/student/controller/PositionController.java
+++ localtest/src/main/java/com/hb/test/student/controller/PositionController.java
@@ -1,14 +1,17 @@
 package com.hb.test.student.controller;
 
+import com.hb.test.student.model.Position;
 import com.hb.test.student.service.BaseService;
 import com.hb.test.student.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.io.Serializable;
+
+@Controller
 @RequestMapping("/position")
-public class PositionController extends BaseController {
+public class PositionController<T extends Position, K extends Integer> extends BaseController<T, K> {
 
     @Autowired
     private PositionService positionService;
