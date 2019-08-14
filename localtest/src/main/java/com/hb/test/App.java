0edd49855571fa1fde926aa296bfcb8ad7c368e5
tuboshu1986 <zzhbce321@163.com>
2019-08-14 19:00:06 +0800

    tmp

--- localtest/src/main/java/com/hb/test/App.java
+++ localtest/src/main/java/com/hb/test/App.java
@@ -1,13 +1,13 @@
 package com.hb.test;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
