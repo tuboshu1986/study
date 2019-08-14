0b7a89a9fa2d2d20149a309780bf9ff50a63282e
tuboshu1986 <zzhbce321@163.com>
2019-08-14 17:47:10 +0800

    tmp

---
+++ localtest/src/main/java/com/moko/MokoApplication.java
@@ -0,0 +1,11 @@
+package com.moko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MokoApplication.class, args);
+    }
+}
